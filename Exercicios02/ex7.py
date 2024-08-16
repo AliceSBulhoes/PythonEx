683bf6fef9f58f97898f8b07954fb8fc4b7abb28
Alice <alicesb2006@gmail.com>
2024-08-16 09:58:28 -0300

    ...

--- Exercicios02/ex7.py
+++ Exercicios02/ex7.py
@@ -2,6 +2,7 @@
     Crie uma função que aceite um parâmetro nomeado itens que seja uma lista de
     strings. A função deve imprimir cada item da lista em linhas separadas.
 """
+
 import os
 
 def listar_itens(itens: list = [], nome: str = "Não Informado") -> None:
